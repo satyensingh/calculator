f161521f9872725be039cb6874aa9142a510d259
Satyen Singh  🇮🇳 <satyen.167@gmail.com>
2018-05-06 21:42:43 +0530

    First commit

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function addNumbers(num1, num2){
+    return num1+num2;
+}
+function subNumbers(num1, num2){
+    return num1-num2;
+}
+function mulNumbers(num1, num2){
+    return num1*num2;
+}
+function divNumbers(num1, num2){
+    return num1/num2;
+}
+
+exports.add = addNumbers;
+exports.sub = subNumbers;
+exports.mul = mulNumbers;
+exports.div = divNumbers;
